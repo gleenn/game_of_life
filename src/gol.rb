8f233c22aebd3e799ffec875e981b4384fbf7b52
Glenn Jahnke <gleenn@gmail.com>
2015-09-07 22:46:33 -0700

    using for-loops instead

--- src/gol.rb
+++ src/gol.rb
@@ -4,30 +4,15 @@ class GoL
   end
 
 	def go(n)
-    current = '1'.rjust(@size, '0')
-    results = [current]
-    (n-1).times do
-      current = next_iteration(current)
-      results << current
+    results = ['1'.rjust(@size, '0')]
+    for i in 0...(n-1)
+      chars = results.last.split('')
+      next_cell = ''
+      for j in 0...(chars.size)
+        next_cell << MAP["#{j==0?'0':chars[j-1]}#{chars[j]}#{chars.fetch(j+1,'0')}"]
+      end
+      results << next_cell
     end
     results
   end
-
-  MAP = {
-      '000' => '0',
-      '001' => '1',
-      '010' => '1',
-      '011' => '1',
-      '100' => '1',
-      '101' => '1',
-      '110' => '1',
-      '111' => '0',
-  }
-
-  def next_iteration(str)
-    cells = str.split(//) + ['0']
-    cells.zip(['0']+cells,['0', '0']+cells).map{|a,b,c| MAP["#{b}#{c}#{a}"]}.tap{|a|a.shift}.join
-  end
 end
-
-
